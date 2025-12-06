f67fe5578aa2f2e6d6d1e79a45168e09d36c11b1
Puk06 <86549420+puk06@users.noreply.github.com>
2025-12-06 23:24:14 +0900

    アクセス修飾子の変更

--- AvatarExplorer.UI/Localization/Localizer.cs
+++ AvatarExplorer.UI/Localization/Localizer.cs
@@ -4,18 +4,18 @@ using System.Text.Json;
 
 namespace AvatarExplorer.UI.Localization;
 
-public class Localizer
+internal class Localizer
 {
     private readonly Dictionary<string, string> _map;
 
-    public static Localizer Instance { get; private set; } = new Localizer();
+    internal static Localizer Instance { get; private set; } = new Localizer();
 
     private Localizer()
     {
         _map = new Dictionary<string, string>();
     }
 
-    public void LoadFromFile(string path)
+    internal void LoadFromFile(string path)
     {
         if (!File.Exists(path)) return;
 
@@ -32,18 +32,18 @@ public class Localizer
         }
     }
 
-    public string GetDisplayName(string internalId)
+    internal string GetDisplayName(string internalId)
     {
         return _map.TryGetValue(internalId, out var name) ? name : internalId;
     }
 
-    public string GetDisplayName(string internalId, string[] args)
+    internal string GetDisplayName(string internalId, string[] args)
     {
         var localizedText = _map.TryGetValue(internalId, out var name) ? name : internalId;
         return args.Length > 0 ? string.Format(localizedText, args) : localizedText;
     }
 
-    public string? GetInternalId(string displayName)
+    internal string? GetInternalId(string displayName)
     {
         foreach (var kv in _map)
         {
@@ -53,5 +53,5 @@ public class Localizer
         return null;
     }
 
-    public IReadOnlyDictionary<string, string> All => _map;
+    internal IReadOnlyDictionary<string, string> All => _map;
 }
