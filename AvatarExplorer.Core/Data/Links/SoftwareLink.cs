12c37ca845be0e4f112aa4295864f136a158c6ab
Puk06 <86549420+puk06@users.noreply.github.com>
2026-01-18 16:42:23 +0900

    アップデートチェックURLの変更

--- AvatarExplorer.Core/Data/Links/SoftwareLink.cs
+++ AvatarExplorer.Core/Data/Links/SoftwareLink.cs
@@ -2,6 +2,6 @@ namespace AvatarExplorer.Core.Data.Links;
 
 public static class SoftwareLink
 {
-    public const string UpdateCheckURL = "https://update.pukosrv.net/check/avatarexplorerv2";
+    public const string UpdateCheckURL = "https://update.pukosrv.net/check/avatarexplorer";
     public const string LatestReleasePageURL = "https://github.com/puk06/VRC-Avatar-Explorer/releases/latest";
 }
