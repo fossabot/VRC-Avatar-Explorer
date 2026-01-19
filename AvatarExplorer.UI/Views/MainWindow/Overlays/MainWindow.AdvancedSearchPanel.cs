f75f629e019d76db31131f4a87f7de2afd131971
Puk06 <86549420+puk06@users.noreply.github.com>
2026-01-20 03:42:55 +0900

    詳細検索処理の修正
    - タグが２回追加されていたのを修正

--- AvatarExplorer.UI/Views/MainWindow/Overlays/MainWindow.AdvancedSearchPanel.cs
+++ AvatarExplorer.UI/Views/MainWindow/Overlays/MainWindow.AdvancedSearchPanel.cs
@@ -9,6 +9,7 @@ public partial class MainWindow
 {
     private void AdvancedSearchPanel_ApplyValues(SearchFilter searchFilter)
     {
+        searchFilter.IsOrSearch = AdvancedSearchPanel_OrSearch.IsChecked ?? false;
         searchFilter.Titles.AddRange(TextParser.Parse(AdvancedSearchPanel_Title.Text ?? string.Empty));
         searchFilter.Authors.AddRange(TextParser.Parse(AdvancedSearchPanel_Author.Text ?? string.Empty));
         searchFilter.BoothIds.AddRange(TextParser.Parse(AdvancedSearchPanel_Booth.Text ?? string.Empty));
@@ -21,7 +22,5 @@ public partial class MainWindow
         searchFilter.NotImplementedAvatars.AddRange(TextParser.Parse(AdvancedSearchPanel_NotImplemented.Text ?? string.Empty));
         searchFilter.Tags.AddRange(TextParser.Parse(AdvancedSearchPanel_Tag.Text ?? string.Empty));
         searchFilter.CommonAvatars.AddRange(TextParser.Parse(AdvancedSearchPanel_CommonAvatar.Text ?? string.Empty));
-        searchFilter.Tags.AddRange(TextParser.Parse(AdvancedSearchPanel_Tag.Text ?? string.Empty));
-        searchFilter.IsOrSearch = AdvancedSearchPanel_OrSearch.IsChecked ?? false;
     }
 }
