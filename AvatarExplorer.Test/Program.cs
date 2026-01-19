709553e81308a21befccd18ac6af5af85cce69cc
Puk06 <86549420+puk06@users.noreply.github.com>
2026-01-19 10:02:45 +0900

    テスト用プロジェクトの追加

---
+++ AvatarExplorer.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using AvatarExplorer.Core.Services;
+
+// このプロジェクトは、UIを作る前に一度機能をテストするためのプレイグラウンドです。.gitignoreで無視されているため、テスト用にお使いください。
+AvatarExplorerApp avatarExplorer = new();
+
+#region Initializing
+avatarExplorer.LoadItemDatabase();
+avatarExplorer.LoadCommonAvatarDatabase();
+avatarExplorer.LoadRuntimeSettings();
+
+Console.WriteLine(string.Format("VRC Avatar Explorer ({0}) Launched!", AvatarExplorerApp.CurrentVersion));
+Console.WriteLine("--------------------------------------------------------------------------------------------------");
+Console.WriteLine(string.Format("Current Items count: {0}", avatarExplorer.GetAllItems().Count));
+Console.WriteLine(string.Format("Current Common Avatars count: {0}", avatarExplorer.GetCommonAvatars().Count));
+Console.WriteLine("--------------------------------------------------------------------------------------------------");
+Console.WriteLine("");
+#endregion
+
+// ここからコードを書くことが出来ます
