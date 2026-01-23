d331e8b7160644a0e865cc267f68ac88a2e610ae
Puk06 <86549420+puk06@users.noreply.github.com>
2026-01-23 14:45:24 +0900

    Unitypackageインポート時の処理の変更
    - Unitypackageインポート時にすぐ画面を閉じるのではなく、100%の表記を出してから1秒経って閉じるように

--- AvatarExplorer.Core/Services/FileSystemService.cs
+++ AvatarExplorer.Core/Services/FileSystemService.cs
@@ -90,6 +90,8 @@ public static class FileSystemService
             // ここの3つ目の引数で出力先のアイテムパスをUI側に返してあげる
             reportProgress?.Invoke((LocalizationKey.Processing.Unitypackage.Status.Completed, 100));
 
+            await Task.Delay(1000); // すぐ閉じるのではなく、100%の表記を出してから1秒経って返すようにする
+
             return unityPackagePath;
         });
 
