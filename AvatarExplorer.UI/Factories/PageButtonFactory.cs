3bbef1fd556650247c09f64a03fdb1fbb531dd28
Puk06 <86549420+puk06@users.noreply.github.com>
2026-01-23 15:48:40 +0900

    リファクタリング

--- AvatarExplorer.UI/Factories/PageButtonFactory.cs
+++ AvatarExplorer.UI/Factories/PageButtonFactory.cs
@@ -6,6 +6,8 @@ using AvatarExplorer.Core.Localization;
 using AvatarExplorer.Core.Models;
 using AvatarExplorer.UI.Localization;
 using AvatarExplorer.UI.Models;
+using Material.Icons;
+using Material.Icons.Avalonia;
 
 namespace AvatarExplorer.UI.Factories;
 
@@ -14,84 +16,60 @@ internal static class PageButtonFactory
     private const string ButtonClass = "button";
     private const string PageButtonClass = "pagebutton";
 
-    // TODO: 後でメソッド分ける
     internal static void AddPageButton(StackPanel parent, ItemTagState itemTagState, int currentPageValue, int itemsPerPage, int totalItemCount, EventHandler<RoutedEventArgs>? onClick = null)
     {
         int totalPages = (int)Math.Ceiling((double)totalItemCount / itemsPerPage);
+        if (totalPages <= 0) return;
 
-        int start = 0;
-        int end = 0;
+        Panel pageInfoPanel = new();
 
-        bool isValidPage = currentPageValue >= 0 && currentPageValue < totalPages;
+        var pageInfo = CreatePageInfoPanel(currentPageValue, totalPages, itemsPerPage, totalItemCount);
+        
+        Grid pageButtonGrid = new() { ColumnDefinitions = new("*,*,*,*"), ColumnSpacing = 10, Margin = new(50, 0, 50, 0) };
+        AddNavigationButtons(pageButtonGrid, itemTagState, currentPageValue, totalPages, onClick);
+        
+        pageInfoPanel.Children.Add(pageButtonGrid);
+        pageInfoPanel.Children.Add(pageInfo);
 
-        if (isValidPage)
-        {
-            start = (currentPageValue * itemsPerPage) + 1;
-            end = Math.Min(start + itemsPerPage - 1, totalItemCount);
-        }
+        parent.Children.Add(pageInfoPanel);
+    }
 
-        bool renderFirstButton = currentPageValue > 0;
-        bool renderBackButton = currentPageValue > 0;
-        bool renderNextButton = currentPageValue < totalPages - 1;
-        bool renderLastButton = currentPageValue < totalPages - 1;
+    private static StackPanel CreatePageInfoPanel(int currentPage, int totalPages, int itemsPerPage, int totalCount)
+    {
+        StackPanel panel = new() { Orientation = Orientation.Vertical, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
 
-        Grid pageGrid = new() { ColumnDefinitions = new("*,*,*,*"), ColumnSpacing = 10, Margin = new(50, 0, 50, 0) };
+        string pageText = Localizer.Instance.GetDisplayName(LocalizationKey.UI.ItemWindow.Page, [(currentPage + 1).ToString(), totalPages.ToString()]);
+        panel.Children.Add(new TextBlock { Text = pageText, FontSize = 15, HorizontalAlignment = HorizontalAlignment.Center });
 
-        StackPanel pageInfoStackPanel = new() { Orientation = Orientation.Vertical, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
+        int start = (currentPage * itemsPerPage) + 1;
+        int end = Math.Min(start + itemsPerPage - 1, totalCount);
+        string rangeText = Localizer.Instance.GetDisplayName(LocalizationKey.UI.ItemWindow.PageItemCount, [start.ToString(), end.ToString(), totalCount.ToString()]);
+        panel.Children.Add(new TextBlock { Text = rangeText, FontSize = 15, HorizontalAlignment = HorizontalAlignment.Center });
 
-        Grid.SetColumn(pageInfoStackPanel, 0);
-        Grid.SetColumnSpan(pageInfoStackPanel, 4);
+        return panel;
+    }
 
-        string pageTextString = Localizer.Instance.GetDisplayName(LocalizationKey.UI.ItemWindow.Page, [(currentPageValue + 1).ToString(), totalPages.ToString()]);
-        TextBlock pageTextBlock = new() { Text = pageTextString, FontSize = 15, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
-        pageInfoStackPanel.Children.Add(pageTextBlock);
+    private static void AddNavigationButtons(Grid grid, ItemTagState state, int current, int total, EventHandler<RoutedEventArgs>? onClick)
+    {
+        if (current > 0) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.FirstPage), HorizontalAlignment.Right, 0, new(state, PageButtonState.First, 0), onClick));
+        if (current > 0) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.ChevronLeft), HorizontalAlignment.Left, 1, new(state, PageButtonState.Back, current - 1), onClick));
+        if (current < total - 1) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.ChevronRight), HorizontalAlignment.Right, 2, new(state, PageButtonState.Next, current + 1), onClick));
+        if (current < total - 1) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.LastPage), HorizontalAlignment.Left, 3, new(state, PageButtonState.Last, total - 1), onClick));
+    }
 
-        string itemsCountTextString = Localizer.Instance.GetDisplayName(LocalizationKey.UI.ItemWindow.PageItemCount, [start.ToString(), end.ToString(), totalItemCount.ToString()]);
-        TextBlock itemsCountTextBlock = new() { Text = itemsCountTextString, FontSize = 15, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
-        pageInfoStackPanel.Children.Add(itemsCountTextBlock);
+    private static MaterialIcon GetMaterialIcon(MaterialIconKind materialIconKind, double size = 25)
+    {
+        return new() { Kind = materialIconKind, Width = size, Height = size };
+    }
 
-        pageGrid.Children.Add(pageInfoStackPanel);
+    private static Button CreateButton(MaterialIcon content, HorizontalAlignment align, int column, PageButtonInfo info, EventHandler<RoutedEventArgs>? onClick)
+    {
+        Button button = new() { Content = content, HorizontalAlignment = align, Tag = info };
+        button.Classes.AddRange([ButtonClass, PageButtonClass]);
+        if (onClick != null) button.Click += onClick;
 
-        if (renderFirstButton)
-        {
-            Button firstButton = new() { Content = "<<", HorizontalAlignment = HorizontalAlignment.Right, Tag = new PageButtonInfo(itemTagState, PageButtonState.First, 0) };
-            firstButton.Classes.AddRange([ButtonClass, PageButtonClass]);
-            
-            Grid.SetColumn(firstButton, 0);
-            if (onClick != null) firstButton.Click += onClick;
-            pageGrid.Children.Add(firstButton);
-        }
+        Grid.SetColumn(button, column);
 
-        if (renderBackButton)
-        {
-            Button backButton = new() { Content = "<", HorizontalAlignment = HorizontalAlignment.Left, Tag = new PageButtonInfo(itemTagState, PageButtonState.Back, currentPageValue - 1) };
-            backButton.Classes.AddRange([ButtonClass, PageButtonClass]);
-
-            Grid.SetColumn(backButton, 1);
-            if (onClick != null) backButton.Click += onClick;
-            pageGrid.Children.Add(backButton);
-        }
-
-        if (renderNextButton)
-        {
-            Button nextButton = new() { Content = ">", HorizontalAlignment = HorizontalAlignment.Right, Tag = new PageButtonInfo(itemTagState, PageButtonState.Next, currentPageValue + 1) };
-            nextButton.Classes.AddRange([ButtonClass, PageButtonClass]);
-
-            Grid.SetColumn(nextButton, 2);
-            if (onClick != null) nextButton.Click += onClick;
-            pageGrid.Children.Add(nextButton);
-        }
-
-        if (renderLastButton)
-        {
-            Button lastButton = new() { Content = ">>", HorizontalAlignment = HorizontalAlignment.Left, Tag = new PageButtonInfo(itemTagState, PageButtonState.Last, totalPages - 1) };
-            lastButton.Classes.AddRange([ButtonClass, PageButtonClass]);
-
-            Grid.SetColumn(lastButton, 3);
-            if (onClick != null) lastButton.Click += onClick;
-            pageGrid.Children.Add(lastButton);
-        }
-
-        parent.Children.Add(pageGrid);
+        return button;
     }
 }
