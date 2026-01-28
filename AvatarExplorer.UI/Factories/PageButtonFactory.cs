11348721229e9dc915c50ac736a0dac75c41365f
Puk06 <86549420+puk06@users.noreply.github.com>
2026-01-28 15:28:47 +0900

    ページボタンのUIの変更

--- AvatarExplorer.UI/Factories/PageButtonFactory.cs
+++ AvatarExplorer.UI/Factories/PageButtonFactory.cs
@@ -25,7 +25,7 @@ internal static class PageButtonFactory
 
         StackPanel pageInfo = CreatePageInfoPanel(currentPageValue, totalPages, itemsPerPage, totalItemCount);
         
-        Grid pageButtonGrid = new() { ColumnDefinitions = new("*,*,*,*"), ColumnSpacing = 10, Margin = new(50, 0, 50, 0) };
+        Grid pageButtonGrid = new() { ColumnDefinitions = new("Auto,Auto,*,Auto,Auto"), ColumnSpacing = 10, Margin = new(15, 0, 15, 0) };
         AddNavigationButtons(pageButtonGrid, itemTagState, currentPageValue, totalPages, onClick);
         
         pageInfoPanel.Children.Add(pageButtonGrid);
@@ -51,10 +51,10 @@ internal static class PageButtonFactory
 
     private static void AddNavigationButtons(Grid grid, ItemTagState state, int current, int total, EventHandler<RoutedEventArgs>? onClick)
     {
-        if (current > 0) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.FirstPage), HorizontalAlignment.Right, 0, new(state, PageButtonState.First, 0), onClick));
-        if (current > 0) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.ChevronLeft), HorizontalAlignment.Left, 1, new(state, PageButtonState.Back, current - 1), onClick));
-        if (current < total - 1) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.ChevronRight), HorizontalAlignment.Right, 2, new(state, PageButtonState.Next, current + 1), onClick));
-        if (current < total - 1) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.LastPage), HorizontalAlignment.Left, 3, new(state, PageButtonState.Last, total - 1), onClick));
+        if (current > 0) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.FirstPage), 0, new(state, PageButtonState.First, 0), onClick));
+        if (current > 0) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.ChevronLeft), 1, new(state, PageButtonState.Back, current - 1), onClick));
+        if (current < total - 1) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.ChevronRight), 3, new(state, PageButtonState.Next, current + 1), onClick));
+        if (current < total - 1) grid.Children.Add(CreateButton(GetMaterialIcon(MaterialIconKind.LastPage), 4, new(state, PageButtonState.Last, total - 1), onClick));
     }
 
     private static MaterialIcon GetMaterialIcon(MaterialIconKind materialIconKind, double size = 25)
@@ -62,9 +62,9 @@ internal static class PageButtonFactory
         return new() { Kind = materialIconKind, Width = size, Height = size };
     }
 
-    private static Button CreateButton(MaterialIcon content, HorizontalAlignment align, int column, PageButtonInfo info, EventHandler<RoutedEventArgs>? onClick)
+    private static Button CreateButton(MaterialIcon content, int column, PageButtonInfo info, EventHandler<RoutedEventArgs>? onClick)
     {
-        Button button = new() { Content = content, HorizontalAlignment = align, Tag = info };
+        Button button = new() { Content = content, HorizontalAlignment = HorizontalAlignment.Center, Tag = info };
         button.Classes.AddRange([ButtonClass, PageButtonClass]);
         if (onClick != null) button.Click += onClick;
 
