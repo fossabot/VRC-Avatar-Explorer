eca5e4b9003dc665dfc6a983acb02106b06d81cf
Puk06 <86549420+puk06@users.noreply.github.com>
2026-02-01 15:48:47 +0900

    自動アップデート処理の修正

--- AvatarExplorer.UI/Views/MainWindow.axaml.cs
+++ AvatarExplorer.UI/Views/MainWindow.axaml.cs
@@ -72,7 +72,7 @@ public partial class MainWindow : Window
         // Scheme Check (Only Windows)
         if (ProcessUtils.IsWindows()) await CheckSchemeAsync();
 
-        await UpdateDialogOverlay_CheckAsync();
+        if (_userPreferences.CheckForUpdate) await UpdateDialogOverlay_CheckAsync();
     }
 
     public async Task SetApplicationArgs(string[]? args)
