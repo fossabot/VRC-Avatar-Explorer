de57dfe35ea001a00fe0075132a26c4376649ee2
Puk06 <86549420+puk06@users.noreply.github.com>
2026-02-01 17:36:07 +0900

    不要な翻訳キーの削除

--- AvatarExplorer.Core/Localization/LocalizationKey.cs
+++ AvatarExplorer.Core/Localization/LocalizationKey.cs
@@ -122,12 +122,6 @@ public static class LocalizationKey
                 public const string NoUpdateAvailable = "UI.Dialog.Update.NoUpdateAvailable";
             }
 
-            public static class FirstLaunch
-            {
-                public const string DialogTitle = "UI.Dialog.FirstLaunch.DialogTitle";
-                public const string DialogMessage = "UI.Dialog.FirstLaunch.DialogMessage";
-            }
-
             public const string SelectSaveFilePath = "UI.Dialog.SelectSaveFilePath";
             public const string SelectFolderPath = "UI.Dialog.SelectFolderPath";
             public const string SelectFilePath = "UI.Dialog.SelectFilePath";
